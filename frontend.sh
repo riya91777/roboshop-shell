c6f136e53fbe6e9d0b42bb34e46969e1ddd6b423
radhakrishna <radhakrishna_p@hotmail.com>
2023-04-20 15:54:54 +0530

    Create Frontend Script

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,6 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-vim /etc/nginx/default.d/roboshop.conf
 
 # some file need to be created manually.
 
